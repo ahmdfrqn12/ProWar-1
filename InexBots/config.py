2b6b47125eee25fa26ef46e2c75017008a73740e
ahmdfrqn12 <52606998+ahmdfrqn12@users.noreply.github.com>
2019-09-09 23:07:08 +0700

    Update config.py

--- InexBots/config.py
+++ InexBots/config.py
@@ -30,10 +30,10 @@ class Config(object):
     }
 
     APP_TYPE    = ApplicationType._VALUES_TO_NAMES[400]
-    APP_VER     = '5.5.5'
+    APP_VER     = '5.9.0'
     CARRIER     = '51089, 1-0'
     SYSTEM_NAME = 'By.InexBots'
-    SYSTEM_VER  = '11.2.5'
+    SYSTEM_VER  = '5.9.0'
     IP_ADDR     = '8.8.8.8'
     EMAIL_REGEX = re.compile(r"[^@]+@[^@]+\.[^@]+")
 
